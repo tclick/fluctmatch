0e0d08c3fb6099b9286fe8b73537a8ba6166528a
Timothy Click <tclick@alumni.ou.edu>
2023-07-18 15:15:36 -0500

    feat: add ENM for C-alpha only

---
+++ src/fluctmatch/core/models/calpha.py
@@ -0,0 +1,83 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2023 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportInvalidTypeVarUse=false, reportOptionalMemberAccess=false, reportGeneralTypeIssues=false
+# pyright: reportOptionalIterable=false
+"""Elastic network model using C-alpha atoms of a protein."""
+
+from types import MappingProxyType
+from typing import TypeVar
+
+from MDAnalysis.core.topologyattrs import Bonds
+
+from ..base import ModelBase
+
+TModel = TypeVar("TModel", bound="Model")
+
+
+class Model(ModelBase):
+    """Universe defined by the protein C-alpha."""
+
+    model = "CALPHA"
+    description = "C-alpha of a protein"
+
+    def __init__(
+        self: TModel,
+        *,
+        xplor: bool = True,
+        extended: bool = True,
+        com: bool = True,
+        guess_angles: bool = False,
+        rmin: float = 0.0,
+        rmax: float = 10.0,
+    ) -> None:
+        super().__init__(
+            xplor=xplor,
+            extended=extended,
+            com=com,
+            guess_angles=guess_angles,
+            rmin=rmin,
+            rmax=rmax,
+        )
+
+        self._mapping = MappingProxyType({"CA": "calpha", "ions": "bioion"})
+        self._selection = MappingProxyType({"CA": "protein", "ions": "bioion"})
+
+    def _add_bonds(self: TModel) -> None:
+        bonds: list[tuple[int, int]] = []
+
+        # Create bonds between C-alphas in adjacent residues
+        for segment in self._universe.segments:
+            atom_selection: str = getattr(self._mapping, self._mapping.items()[0])
+            atoms = segment.atoms.select_atoms(atom_selection)
+            bonds.extend(tuple(zip(atoms.ix_array[1:], atoms.ix_array[:-1], strict=False)))  # ignore: PD007
+        self._universe.add_TopologyAttr(Bonds(bonds))
