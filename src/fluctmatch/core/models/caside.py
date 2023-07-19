0a503b7fb8aa83de403ff07dbf8ae6f6d4d75b27
Timothy Click <tclick@alumni.ou.edu>
2023-07-18 21:09:33 -0500

    feat: add two new models
    Add model to represent C-alpha and the sidechain and a model to represent N-sidechain-C.

---
+++ src/fluctmatch/core/models/caside.py
@@ -0,0 +1,91 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2023 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportInvalidTypeVarUse=false, reportOptionalMemberAccess=false, reportGeneralTypeIssues=false
+# pyright: reportOptionalIterable=false
+# flake8: noqa
+"""Class definition for beads using C-alpha and C-beta positions"""
+
+from types import MappingProxyType
+from typing import TypeVar
+
+from MDAnalysis.core.topologyattrs import Bonds
+
+from ..base import ModelBase
+from ..selection import *
+
+TModel = TypeVar("TModel", bound="Model")
+
+
+class Model(ModelBase):
+    """Universe consisting of the C-alpha and sidechains of a protein."""
+
+    model = "CASIDE"
+    description = "C-alpha and sidechain (c.o.m./c.o.g.) of protein"
+
+    def __init__(
+        self: TModel,
+        *,
+        xplor: bool = True,
+        extended: bool = True,
+        com: bool = True,
+        guess_angles: bool = False,
+        rmin: float = 0.0,
+        rmax: float = 10.0,
+    ) -> None:
+        super().__init__(
+            xplor=xplor,
+            extended=extended,
+            com=com,
+            guess_angles=guess_angles,
+            rmin=rmin,
+            rmax=rmax,
+        )
+
+        self._mapping = MappingProxyType({"CA": "calpha", "CB": "hsidechain and not name H*", "ions": "bioion"})
+        self._selection = MappingProxyType({"CA": "hbackbone", "CB": "hsidechain", "ions": "bioion"})
+
+    def _add_bonds(self: TModel) -> NoReturn:
+        bonds: list[tuple[int, int]] = []
+
+        # Create bonds intraresidue C-alpha and C-beta atoms.
+        residues = self._universe.select_atoms("protein and not resname GLY").residues
+        atom1: AtomGroup = residues.atoms.select_atoms("calpha")
+        atom2: AtomGroup = residues.atoms.select_atoms("cbeta")
+        bonds.extend(tuple(zip(atom1.ix_array, atom2.ix_array, strict=True)))
+
+        # Create interresidue C-alpha bonds within a segment
+        for segment in self._universe.segments:
+            atoms: AtomGroup = segment.atoms.select_atoms("calpha")
+            bonds.extend(tuple(zip(atoms.ix_array[1:], atoms.ix_array[:-1], strict=True)))
+
+        self._universe.add_TopologyAttr(Bonds(bonds))
---
+++ src/fluctmatch/core/models/ncsc.py
@@ -0,0 +1,123 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2023 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportInvalidTypeVarUse=false, reportOptionalMemberAccess=false, reportGeneralTypeIssues=false
+# pyright: reportOptionalIterable=false
+# flake8: noqa
+"""Class definition for beads using N, carboxyl oxygens, and sidechains."""
+
+from types import MappingProxyType
+from typing import TypeVar
+
+import MDAnalysis as mda
+from MDAnalysis.core.topologyattrs import Bonds
+
+from ..base import ModelBase
+from ..selection import *
+
+TModel = TypeVar("TModel", bound="Model")
+
+
+class Model(ModelBase):
+    """Universe consisting of the amine, carboxyl, and sidechain regions."""
+
+    model = "NCSC"
+    description = "c.o.m./c.o.g. of N, O, and sidechain of protein"
+
+    def __init__(
+        self: TModel,
+        *,
+        xplor: bool = True,
+        extended: bool = True,
+        com: bool = True,
+        guess_angles: bool = False,
+        rmin: float = 0.0,
+        rmax: float = 10.0,
+    ) -> None:
+        super().__init__(
+            xplor=xplor,
+            extended=extended,
+            com=com,
+            guess_angles=guess_angles,
+            rmin=rmin,
+            rmax=rmax,
+        )
+
+        self._mapping: MappingProxyType = MappingProxyType(
+            {
+                "N": "protein and name N",
+                "CB": "hsidechain and not name H*",
+                "O": "protein and name O OT1 OT2 OXT",
+                "ions": "bioion",
+            }
+        )
+        self._selection: MappingProxyType = MappingProxyType(
+            {"N": "amine", "CB": "hsidechain", "O": "carboxyl", "ions": "bioion"}
+        )
+
+    def _add_bonds(self: TModel) -> None:
+        bonds: List[Tuple[int, int]] = []
+
+        # Create bonds intraresidue atoms
+        residues = self._universe.select_atoms("protein").residues
+        atom1: AtomGroup = residues.atoms.select_atoms("name N")
+        atom2: AtomGroup = residues.atoms.select_atoms("name O")
+        bonds.extend(tuple(zip(atom1.ix_array, atom2.ix_array, strict=True)))
+
+        residues = residues.atoms.select_atoms("not resname GLY").residues
+        atom1: AtomGroup = residues.atoms.select_atoms("name N")
+        atom2: AtomGroup = residues.atoms.select_atoms("name O")
+        atom3: AtomGroup = residues.atoms.select_atoms("cbeta")
+        bonds.extend(tuple(zip(atom1.ix_array, atom2.ix_array, strict=True)))
+        bonds.extend(tuple(zip(atom2.ix_array, atom3.ix_array, strict=True)))
+
+        # Create interresidue bonds
+        for segment in self._universe.segments:
+            atom1: AtomGroup = segment.atoms.select_atoms("name O")
+            atom2: AtomGroup = segment.atoms.select_atoms("name N")
+            bonds.extend(tuple(zip(atom1.ix_array[:-1], atom2.ix_array[1:], strict=True)))
+
+        self._universe.add_TopologyAttr(Bonds(bonds))
+
+    def _add_masses(self: TModel, universe: mda.Universe) -> None:
+        super()._add_masses(universe)
+        amine: AtomGroup = self._universe.select_atoms(self._mapping["N"])
+        carboxyl: AtomGroup = self._universe.select_atoms(self._mapping["O"])
+        amine.masses += 0.5 * self._universe.select_atoms("hcalpha").total_mass()
+        carboxyl.masses += 0.5 * self._universe.select_atoms("hcalpha").total_mass()
+
+    def _add_charges(self: TModel, universe: mda.Universe) -> None:
+        super()._add_charges(universe)
+        amine: AtomGroup = self._universe.select_atoms(self._mapping["N"])
+        carboxyl: AtomGroup = self._universe.select_atoms(self._mapping["O"])
+        amine.charges += 0.5 * self._universe.select_atoms("hcalpha").total_charge()
+        carboxyl.charges += 0.5 * self._universe.select_atoms("hcalpha").total_charge()
