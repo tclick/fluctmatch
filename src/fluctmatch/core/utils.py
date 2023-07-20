aab1b2e7caa4430e98515f64e34c19bc3c3fd655
Timothy Click <tclick@alumni.ou.edu>
2023-07-20 17:30:10 -0500

    chore: ignore pyright warning

--- src/fluctmatch/core/utils.py
+++ src/fluctmatch/core/utils.py
@@ -30,7 +30,7 @@
 #  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 #  DAMAGE.
 # ------------------------------------------------------------------------------
-# pyright: reportGeneralTypeIssues=false
+# pyright: reportInvalidTypeVarUse=false, reportGeneralTypeIssues=false
 """Various utilities for the models."""
 
 import string
