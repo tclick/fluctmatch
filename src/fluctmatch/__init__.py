538c03c98fd7e7496e2424914124b40bbcb96147
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-04-29 20:34:50 -0500

    feat: add copyright information and configure the primary logger
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- src/fluctmatch/__init__.py
+++ src/fluctmatch/__init__.py
@@ -31,3 +31,78 @@
 #  DAMAGE.
 # ------------------------------------------------------------------------------
 """Fluctuation Matching."""
+
+import getpass
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING, ParamSpec, TypeVar
+
+from click_loguru import ClickLoguru
+from loguru import logger
+
+if TYPE_CHECKING:
+    from loguru import Logger
+else:
+    from loguru import logger as Logger  # noqa: N812
+
+NAME = "fluctmatch"
+T = TypeVar("T")
+P = ParamSpec("P")
+__version__: str = "4.0.0a0"
+__copyright__: str = """Copyright (C) 2013-2024 Timothy H. Click <thclick@umary.edu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+logger.remove()
+
+click_loguru = ClickLoguru(
+    NAME,
+    __version__,
+    log_dir_parent=Path.cwd() / "logs",
+    timer_log_level="info",
+)
+
+
+def config_logger(name: str, logfile: str = "fluctmatch.log", level: str = "INFO") -> Logger:
+    """Configure logger.
+
+    Parameters
+    ----------
+    name : str
+        name associated with the logger
+    logfile: str
+        name of log file
+    level : str
+        minimum level for logging
+
+    Returns
+    -------
+    Logger
+        logging object
+    """
+    config = {
+        "handlers": [
+            {
+                "sink": sys.stderr,
+                "format": "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+                "colorize": True,
+                "level": level,
+                "backtrace": True,
+                "diagnose": True,
+            },
+            {"sink": logfile, "format": "{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", "level": level},
+        ],
+    }
+    logger.configure(**config)
+    return logger.bind(name=name, user=getpass.getuser())
