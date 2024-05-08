117c3ab613cfe3abaecfc34098d8da0dc8e8e5af
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-05-08 09:34:22 -0500

    feat: split trajectory into smaller units
    Use the output from the setup subcommand to create smaller trajectories.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

---
+++ src/fluctmatch/commands/split.py
@@ -0,0 +1,150 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportAttributeAccessIssue = false
+"""Split a trajectory into smaller trajectories."""
+
+from __future__ import annotations
+
+import json
+from pathlib import Path
+
+import click
+import MDAnalysis as mda
+from click_help_colors import HelpColorsCommand
+
+from fluctmatch import __copyright__
+from fluctmatch.libs.logging import config_logger
+
+
+@click.command(
+    cls=HelpColorsCommand,
+    help=f"{__copyright__}\nAlign a trajectory.",
+    short_help="Split a trajectory into smaller trajectories.",
+    help_headers_color="yellow",
+    help_options_color="blue",
+    context_settings={"max_content_width": 120},
+)
+@click.option(
+    "-s",
+    "--topology",
+    metavar="FILE",
+    default=Path.cwd().joinpath("input.parm7"),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path),
+    help="Topology file",
+)
+@click.option(
+    "-f",
+    "--trajectory",
+    metavar="FILE",
+    default=Path.cwd().joinpath("input.nc"),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path),
+    help="Trajectory file",
+)
+@click.option(
+    "--json",
+    "windows_input",
+    metavar="JSON",
+    show_default=True,
+    default=Path.cwd().joinpath("setup.json"),
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="JSON file",
+)
+@click.option(
+    "-o",
+    "--outfile",
+    metavar="FILE",
+    default="cg.dcd",
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Trajectory output file",
+)
+@click.option(
+    "-l",
+    "--logfile",
+    metavar="WARNING",
+    show_default=True,
+    default=Path.cwd() / Path(__file__).with_suffix(".log"),
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Path to log file",
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    default="INFO",
+    show_default=True,
+    help="Minimum severity level for log messages",
+)
+@click.help_option("-h", "--help", help="Show this help message and exit")
+def split(
+    topology: Path,
+    trajectory: Path,
+    outfile: Path,
+    windows_input: Path,
+    logfile: Path,
+    verbosity: str,
+) -> None:
+    """Split a trajectory into smaller trajectories using the JSON file created during setup.
+
+    Parameters
+    ----------
+    topology : Path, default=$CWD/input.parm7
+        Topology file
+    trajectory : Path, default=$CWD/input.nc
+        Trajectory file
+    outfile : Path, default=cg.dcd
+        Trajectory output file
+    logfile : Path, default=$CWD/setup.log
+        Location of log file
+    windows_input : Path, default=$CWD/setup.json
+        JSON file
+    verbosity : str, default=INFO
+        Level of verbosity for logging output
+    """
+    logger = config_logger(name=__name__, logfile=logfile, level=verbosity)
+    click.echo(__copyright__)
+
+    with windows_input.open() as f:
+        setup_input: dict = json.load(f)
+
+    universe = mda.Universe(topology, trajectory)
+    n_atoms = universe.atoms.n_atoms
+
+    logger.info("Splitting trajectory into smaller trajectories...")
+    info = ((Path(outdir) / outfile, data["start"], data["stop"]) for outdir, data in setup_input.items())
+    for traj_file, start, stop in info:
+        with mda.Writer(traj_file.as_posix(), n_atoms=n_atoms) as writer:
+            logger.debug(f"Spliting trajectory {start} to {stop} in {traj_file}.")
+            for _ in universe.trajectory[start:stop]:
+                writer.write(universe.atoms)
---
+++ tests/commands/test_split.py
@@ -0,0 +1,104 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+"""Tests for split command."""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import Self
+
+import pytest
+from click.testing import CliRunner
+from fluctmatch.cli import main
+
+from ..datafile import DCD, PSF
+
+
+class TestSplit:
+    """Run test for split subcommand."""
+
+    @pytest.fixture(scope="class")
+    def cli_runner(self: Self) -> CliRunner:
+        """Fixture for CLI runner.
+
+        Returns
+        -------
+        CliRunner
+            Click CLI runner
+        """
+        return CliRunner()
+
+    def test_help(self: Self, cli_runner: CliRunner) -> None:
+        """Test help output.
+
+        GIVEN the init subcommand
+        WHEN the help option is invoked
+        THEN the help output should be displayed
+
+        Parameters
+        ----------
+        cli_runner : CliRunner
+            CLI runner
+        """
+        result = cli_runner.invoke(main, "split -h")
+
+        assert "Usage:" in result.output
+        assert result.exit_code == os.EX_OK
+
+    def test_split(self: Self, cli_runner: CliRunner) -> None:
+        """Test subcommand in an isolated filesystem.
+
+        GIVEN an output subdirectory
+        WHEN invoking the setup subcommand
+        THEN the subcommand will complete successfully.
+
+
+        Parameters
+        ----------
+        cli_runner : CliRunner
+            CLI runner
+        """
+        with cli_runner.isolated_filesystem() as ifs:
+            tmp_path = Path(ifs)
+            outdir = tmp_path / "test"
+            log_file = outdir / "setup.log"
+            json_file = log_file.with_suffix(".json")
+
+            cli_runner.invoke(main, f"setup -s {PSF} -f {DCD} -o {outdir} --json {json_file}  -w 1000 -l {log_file}")
+            result = cli_runner.invoke(main, f"split -s {PSF} -f {DCD} --json {json_file} -o cg.dcd  -l {log_file}")
+
+            traj = outdir / "01" / "cg.dcd"
+            assert result.exit_code == os.EX_OK
+            assert traj.exists()
+            assert traj.stat().st_size > 0
