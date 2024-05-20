047c51c19dd6f2e3918ad50e7befb4847e2e6a97
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-05-20 09:55:36 -0500

    feat: add model using C-alpha and C-beta atoms
    Use C-alpha for backbone and center of mass for sidechain.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- tests/model/test_protein.py
+++ tests/model/test_protein.py
@@ -36,7 +36,7 @@ from typing import Self
 
 import MDAnalysis as mda
 import pytest
-from fluctmatch.model import calpha  # , caside, ncsc, polar
+from fluctmatch.model import calpha, caside  # , ncsc, polar
 from fluctmatch.model.base import coarse_grain
 from numpy import testing
 
@@ -166,3 +166,105 @@ class TestCalpha:
         # Test trajectory addition
         testing.assert_equal(system.trajectory.n_frames, u.trajectory.n_frames, err_msg="Number of frames not equal")
         testing.assert_allclose(model_positions, atom_positions, err_msg="Positions not equal")
+
+
+class TestCaside:
+    """Test C-alpha/sidechain model."""
+
+    @pytest.fixture(scope="class")
+    def atoms(self: Self, universe: mda.Universe) -> mda.AtomGroup:
+        """Fixture for a C-alpha model from the all-atom model.
+
+        Returns
+        -------
+        MDAnalysis.AtomGroup
+            Atom group with C-alpha atoms and bioions
+        """
+        return universe.select_atoms(f"(protein and name CA CB) or name {BIOION}")
+
+    @pytest.fixture()
+    def model(self: Self, universe: mda.Universe) -> mda.Universe:
+        """Fixture for a C-alpha model.
+
+        Parameters
+        ----------
+        universe : mda.Universe
+            Universe with protein, DNA, and water
+
+        Returns
+        -------
+        C-alpha only universe with coordinates
+        """
+        return coarse_grain.get("CASIDE", universe, guess_angles=True, com=True)
+
+    def test_topology_creation(self: Self, atoms: mda.AtomGroup, model: caside.CasideModel) -> None:
+        """Test topology for C-alpha model.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN number of atoms, residues, masses, and charges should be equal
+        """
+        model.create_topology()
+        system: mda.Universe = model._universe
+
+        testing.assert_equal(system.atoms.n_atoms, atoms.n_atoms, err_msg="Number of atoms not equal")
+        testing.assert_equal(system.residues.n_residues, atoms.n_residues, err_msg="Number of residues not equal")
+        testing.assert_allclose(system.residues.masses, atoms.residues.masses, err_msg="Masses not equal")
+        testing.assert_allclose(system.residues.charges, atoms.residues.charges, err_msg="Charges not equal")
+
+    def test_bond_generation(self: Self, model: calpha.CalphaModel) -> None:
+        """Test the creation of intramolecular bonds between C-alpha atoms.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN bonds are formed between respective sites.
+        """
+        testing.assert_raises(AttributeError, model.generate_bonds)
+
+        model.create_topology()
+        model.generate_bonds()
+        system: mda.Universe = model._universe
+
+        assert len(system.bonds) > 0, "Bonds not generated"
+        assert len(system.angles) > 0, "Angles not generated"
+        assert len(system.dihedrals) > 0, "Dihedral angles not generated"
+        assert len(system.impropers) > 0, "Improper dihedral angles not generated"
+
+    def test_trajectory_addition(self: Self, atoms: mda.AtomGroup, model: calpha.CalphaModel) -> None:
+        """Ensure that positions match between the all-atom and C-alpha model.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN trajectory is added to the universe with the same number of frames.
+        """
+        model.create_topology()
+        model.add_trajectory()
+        u = atoms.universe
+        system: mda.Universe = model._universe
+
+        testing.assert_equal(system.trajectory.n_frames, u.trajectory.n_frames, err_msg="Number of frames not equal")
+
+    def test_transformation(self: Self, atoms: mda.AtomGroup, model: calpha.CalphaModel) -> None:
+        """Test transformation from an all-atom system to C-alpha model.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN the system will have a proper topology and trajectory.
+        """
+        u = atoms.universe
+        system = model.transform()
+
+        # Test topology creation
+        testing.assert_equal(system.atoms.n_atoms, atoms.n_atoms, err_msg="Number of atoms not equal")
+        testing.assert_equal(system.residues.n_residues, atoms.n_residues, err_msg="Number of residues not equal")
+        testing.assert_allclose(system.residues.masses, atoms.residues.masses, err_msg="Masses not equal")
+        testing.assert_allclose(system.residues.charges, atoms.residues.charges, err_msg="Charges not equal")
+
+        # Test bond generation
+        assert len(system.bonds) > 0, "Bonds not generated"
+        assert len(system.angles) > 0, "Angles not generated"
+        assert len(system.dihedrals) > 0, "Dihedral angles not generated"
+        assert len(system.impropers) > 0, "Improper dihedral angles not generated"
+
+        # Test trajectory addition
+        testing.assert_equal(system.trajectory.n_frames, u.trajectory.n_frames, err_msg="Number of frames not equal")
