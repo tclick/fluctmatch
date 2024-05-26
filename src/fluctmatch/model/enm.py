a4553fff37a97559e98292150f9463a3d25b16d4
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-05-26 16:53:15 -0500

    feat: add definition for elastic network model (ENM)
    Transform a coarse-grain model into an elastic network model.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

---
+++ src/fluctmatch/model/enm.py
@@ -0,0 +1,121 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportInvalidTypeVarUse=false, reportOptionalIterable=false, reportAssignmentType=false
+"""Elastic network model using C-alpha atoms of a protein."""
+
+from typing import ClassVar, Self
+
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis.align import AverageStructure
+from MDAnalysis.core.topologyattrs import Angles, Bonds, Charges, Dihedrals, Impropers
+from MDAnalysis.lib.distances import self_capped_distance
+from numpy.typing import NDArray
+
+from fluctmatch.libs.utils import rename_universe
+from fluctmatch.model.base import CoarseGrainModel
+
+
+class ElasticModel(CoarseGrainModel):
+    """Convert a basic coarse-grain universe into an elastic-network model.
+
+    Determines the interactions between beads via distance cutoffs `rmin` and
+    `rmax`. The atoms and residues are also renamed to prevent name collision
+    when working with fluctuation matching.
+    """
+
+    model: ClassVar[str] = "ENM"
+    description: ClassVar[str] = "Elastic network model"
+
+    def __init__(self: Self, mobile: mda.Universe, /, **kwargs: dict[str, bool]) -> None:
+        super().__init__(mobile, **kwargs)
+        self._universe = self._mobile.copy()
+
+    def create_topology(self: Self) -> None:
+        """Determine the topology attributes and initialize the universe."""
+        rename_universe(self._universe)
+
+        self._universe.add_TopologyAttr(Charges(np.zeros_like(self.atoms.charges)))
+
+    def generate_bonds(self: Self, rmin: float = 0.0, rmax: float = 10.0, guess: bool = False) -> None:
+        """Add bonds, angles, dihedrals, and improper dihedrals to the universe.
+
+        Parameters
+        ----------
+        rmin : float
+            Minimum bond distance
+        rmax : bool
+            Maximum bond distance
+        guess : bool, optional
+            Guess angles and dihedral and improper dihedral angles
+        """
+        super().generate_bonds(rmin, rmax, guess)
+        if not guess:
+            self._universe.add_TopologyAttr(Angles([]))
+            self._universe.add_TopologyAttr(Dihedrals([]))
+            self._universe.add_TopologyAttr(Impropers([]))
+
+    def add_trajectory(
+        self: Self, start: int | None = None, stop: int | None = None, step: int | None = None, com: bool = True
+    ) -> None:
+        """Add coordinates to the new system.
+
+        Parameters
+        ----------
+        start : int, optional
+            Beginning frame
+        stop : int, optional
+            Final frame
+        step : int, optional
+            Number of frames to skip
+        com : bool, optional
+            Define positions either by center of mass (default) or of geometry
+        """
+        pass
+
+    def _add_bonds(self: Self, rmin: float, rmax: float) -> None:
+        # Determine the average positions of the system
+        average = AverageStructure(self._universe)
+        average.run()
+
+        # Find bonds with distance range of rmin <= r <= rmax
+        pairs, _ = self_capped_distance(average.results.positions, rmax, min_cutoff=rmin)
+
+        # Include predefined bonds
+        bonds: NDArray = self._universe.bonds.dump_contents() if hasattr(self._universe, "bonds") else []
+        pairs = np.concatenate([pairs, bonds], axis=0)
+
+        bonds = np.unique(pairs, axis=0)
+
+        # Add topology information
+        self._universe.add_TopologyAttr(Bonds(bonds))
---
+++ tests/model/test_enm.py
@@ -0,0 +1,134 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+"""Test elastic network model."""
+
+from typing import Self
+
+import MDAnalysis as mda
+import pytest
+from fluctmatch.model import enm
+from fluctmatch.model.base import coarse_grain
+from numpy import testing
+
+from tests.datafile import DCD, PSF
+
+
+@pytest.fixture(autouse=True, scope="class")
+def universe() -> mda.Universe:
+    """Fixture for a universe.
+
+    Returns
+    -------
+    Universe
+        universe with protein, DNA, and water
+    """
+    return mda.Universe(PSF, DCD)
+
+
+class TestElasticNetworkModel:
+    """Test elastic network model."""
+
+    @pytest.fixture(scope="class")
+    def atoms(self: Self, universe: mda.Universe) -> mda.AtomGroup:
+        """Fixture for a coarse-grain model.
+
+        Returns
+        -------
+        MDAnalysis.AtomGroup
+            Atom group of a coarse-grain model
+        """
+        return universe.select_atoms("all")
+
+    @pytest.fixture()
+    def model(self: Self, universe: mda.Universe) -> enm.ElasticModel:
+        """Fixture for an elastic network model.
+
+        Parameters
+        ----------
+        universe : mda.Universe
+            Coarse-grain model
+
+        Returns
+        -------
+        Elastic network model
+        """
+        return coarse_grain.get("ENM", universe)
+
+    def test_topology_creation(self: Self, atoms: mda.AtomGroup, model: enm.ElasticModel) -> None:
+        """Test topology for elastic network model.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN number of atoms, residues, masses, and charges should be equal
+        """
+        model.create_topology()
+        system: mda.Universe = model.universe
+
+        testing.assert_equal(system.atoms.n_atoms, atoms.n_atoms, err_msg="Number of atoms not equal")
+        testing.assert_equal(system.residues.n_residues, atoms.n_residues, err_msg="Number of residues not equal")
+        testing.assert_allclose(system.residues.masses, atoms.residues.masses, err_msg="Masses not equal")
+        testing.assert_allclose(system.residues.charges, 0.0, err_msg="Charges not equal")
+
+    def test_bond_generation(self: Self, model: enm.ElasticModel) -> None:
+        """Test the creation of intramolecular bonds between elastic network atoms.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN bonds are formed between respective sites.
+        """
+        model.create_topology()
+        model.generate_bonds(guess=False)
+        system: mda.Universe = model.universe
+
+        assert len(system.bonds) > 0, "Bonds not generated"
+        testing.assert_equal(len(system.impropers), 0, err_msg="Angles generated")
+        testing.assert_equal(len(system.impropers), 0, err_msg="Dihedral angles generated")
+        testing.assert_equal(len(system.impropers), 0, err_msg="Improper dihedral angles generated")
+
+    def test_trajectory_addition(self: Self, atoms: mda.AtomGroup, model: enm.ElasticModel) -> None:
+        """Ensure that positions match between the all-atom and elastic network model.
+
+        GIVEN an all-atom universe
+        WHEN transformed into a coarse-grain model
+        THEN trajectory is added to the universe with the same number of frames.
+        """
+        model.create_topology()
+        model.add_trajectory(com=True)
+        u = atoms.universe
+        system: mda.Universe = model.universe
+
+        atom_positions = [atoms.positions for _ in u.trajectory]
+        model_positions = [system.atoms.positions for _ in system.trajectory]
+
+        testing.assert_equal(system.trajectory.n_frames, u.trajectory.n_frames, err_msg="Number of frames not equal")
+        testing.assert_allclose(model_positions, atom_positions, err_msg="Positions not equal")
