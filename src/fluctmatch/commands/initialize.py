437ba61348c7bd0e4d780bb5b3347ac01d21db8b
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-05-27 16:35:56 -0500

    feat: write initial parameter files for fluctuation matching
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

---
+++ src/fluctmatch/commands/initialize.py
@@ -0,0 +1,183 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportGeneralTypeIssues=false, reportAttributeAccessIssue=false, reportArgumentType=false
+"""Split a trajectory into smaller trajectories."""
+
+from __future__ import annotations
+
+import asyncio
+from pathlib import Path
+
+import click
+import MDAnalysis as mda
+import numpy as np
+from click_help_colors import HelpColorsCommand
+from loguru import logger
+from numpy.typing import NDArray
+from scipy import constants
+
+from fluctmatch import __copyright__
+from fluctmatch.libs import write_files
+from fluctmatch.libs.logging import config_logger
+
+
+@click.command(
+    cls=HelpColorsCommand,
+    help=f"{__copyright__}\nInitialize files for fluctuation matching.",
+    short_help="Initialize files for fluctuation matching.",
+    help_headers_color="yellow",
+    help_options_color="blue",
+    context_settings={"max_content_width": 120},
+)
+@click.option(
+    "-s",
+    "--topology",
+    metavar="FILE",
+    default=Path.cwd().joinpath("cg.psf"),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path),
+    help="Topology file",
+)
+@click.option(
+    "-f",
+    "--trajectory",
+    metavar="FILE",
+    default=Path.cwd().joinpath("cg.dcd"),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, path_type=Path),
+    help="Trajectory file",
+)
+@click.option(
+    "-d",
+    "--dir",
+    "directory",
+    metavar="DIR",
+    default=Path.cwd(),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path),
+    help="Output directory",
+)
+@click.option(
+    "-p",
+    "--prefix",
+    metavar="FILE",
+    default="fluctmatch",
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Filename prefix",
+)
+@click.option(
+    "-l",
+    "--logfile",
+    metavar="WARNING",
+    show_default=True,
+    default=Path.cwd() / Path(__file__).with_suffix(".log"),
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Path to log file",
+)
+@click.option(
+    "-t",
+    "--temp",
+    "temperature",
+    metavar="TEMP",
+    show_default=True,
+    default=300.0,
+    type=click.FloatRange(min=1.0, clamp=True),
+    help="Simulation temperature (in K)",
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    metavar="LEVEL",
+    default="INFO",
+    show_default=True,
+    help="Minimum severity level for log messages",
+)
+@click.help_option("-h", "--help", help="Show this help message and exit")
+def initialize(
+    topology: Path,
+    trajectory: Path,
+    directory: Path,
+    prefix: Path,
+    logfile: Path,
+    temperature: float,
+    verbosity: str,
+) -> None:
+    """Split a trajectory into smaller trajectories using the JSON file created during setup.
+
+    Parameters
+    ----------
+    topology : Path, default=$CWD/cg.psf
+        Topology file
+    trajectory : Path, default=$CWD/cg.dcd
+        Trajectory file
+    directory : Path, default=$CWD
+        Output directory
+    prefix : Path, default=fluctmatch
+        Filename prefix
+    logfile : Path, default=$CWD/fluctmatch.log
+        Location of log file
+    temperature : float, default=300.0
+        Simulation temperature
+    verbosity : str, default=INFO
+        Level of verbosity for logging output
+    """
+    config_logger(name=__name__, logfile=logfile, level=verbosity)
+    click.echo(__copyright__)
+
+    boltzmann: float = temperature * (constants.k * constants.N_A / (constants.calorie * constants.kilo))
+
+    logger.info("Loading the universe.")
+    universe = mda.Universe(topology, trajectory)
+
+    logger.info("Determining the average bond distance and the corresponding bond fluctuations.")
+    distances = np.asarray([universe.bonds.values() for _ in universe.trajectory])
+    lengths: NDArray = distances.mean(axis=0)
+    fluctuations: NDArray = distances.std(axis=0)
+    forces: NDArray = boltzmann / np.square(fluctuations)
+
+    # filenames
+    prm_file = directory / prefix.with_suffix(".prm")
+    rtf_file = prm_file.with_suffix(".rtf")
+    str_file = prm_file.with_suffix(".bonds.str")
+    avg_ic_file = prm_file.with_suffix(".average.ic")
+    fluct_ic_file = prm_file.with_suffix(".fluct.ic")
+
+    loop = asyncio.get_event_loop()
+    tasks = [
+        write_files.write_parameters(universe, forces=forces, distances=lengths, top=rtf_file, par=prm_file),
+        write_files.write_stream(universe, filename=str_file),
+        write_files.write_intcor(universe, lengths, filename=avg_ic_file),
+        write_files.write_intcor(universe, fluctuations, filename=fluct_ic_file),
+    ]
+    loop.run_until_complete(asyncio.gather(*tasks))
---
+++ tests/commands/test_initialize.py
@@ -0,0 +1,113 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+"""Tests for initialization before fluctuation matching."""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import Self
+
+import pytest
+from click.testing import CliRunner
+from fluctmatch.cli import main
+
+from tests.datafile import FLUCTDCD, FLUCTPSF
+
+
+class TestInitialize:
+    """Test file initialization subcommand."""
+
+    @pytest.fixture(scope="class")
+    def cli_runner(self: Self) -> CliRunner:
+        """Fixture for CLI runner.
+
+        Returns
+        -------
+        CliRunner
+            Click CLI runner
+        """
+        return CliRunner()
+
+    def test_help(self: Self, cli_runner: CliRunner) -> None:
+        """Test help output.
+
+        GIVEN the init subcommand
+        WHEN the help option is invoked
+        THEN the help output should be displayed
+
+        Parameters
+        ----------
+        cli_runner : CliRunner
+            Command-line cli_runner
+        """
+        result = cli_runner.invoke(main, "initialize -h")
+
+        assert "Usage:" in result.output
+        assert result.exit_code == os.EX_OK
+
+    def test_initiailization(self: Self, cli_runner: CliRunner) -> None:
+        """Test whether parameter, topology and stream files are written.
+
+        GIVEN a coarse-grain model
+        WHEN given an output directory and filename prefix
+        THEN a CHARMM topology, parameter, and stream file are written
+
+        Parameters
+        ----------
+        cli_runner : CliRunner
+            Command-line cli_runner
+        """
+        with cli_runner.isolated_filesystem() as ifs:
+            tmp_path = Path(ifs)
+            prefix = "cg"
+            filename = tmp_path / prefix
+            log_file = tmp_path / "initialize.log"
+
+            result = cli_runner.invoke(
+                main, f"initialize -s {FLUCTPSF} -f {FLUCTDCD} -l {log_file} -d {tmp_path} -p {prefix}"
+            )
+
+            prm_file = filename.with_suffix(".prm")
+            rtf_file = prm_file.with_suffix(".rtf")
+            str_file = prm_file.with_suffix(".str")
+            ic_file = prm_file.with_suffix(".fluct.ic")
+            assert result.exit_code == os.EX_OK
+            assert prm_file.exists()
+            assert prm_file.stat().st_size > 0
+            assert rtf_file.exists()
+            assert rtf_file.stat().st_size > 0
+            assert str_file.exists()
+            assert str_file.stat().st_size > 0
+            assert ic_file.exists()
+            assert ic_file.stat().st_size > 0
