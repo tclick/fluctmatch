e9c337bac6cf2a6af41c2a768237619bb6be1d81
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-05-29 14:09:43 -0500

    feat: read and write a CHARMM parameter file
    Read, write, or initialize a CHARMM parameter set.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

---
+++ src/fluctmatch/io/charmm_parameter.py
@@ -0,0 +1,231 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+# pyright: reportInvalidTypeVarUse=false, reportGeneralTypeIssues=false
+"""Initialize and write a stream file."""
+
+from pathlib import Path
+from typing import Self
+
+import MDAnalysis as mda
+import numpy as np
+import parmed as pmd
+import parmed.charmm as charmm
+from loguru import logger
+from numpy.typing import NDArray
+
+
+class CharmmParameter:
+    """Initialize and write a CHARMM stream data from bond data."""
+
+    def __init__(self: Self) -> None:
+        """Prepare a CHARMM parameter file."""
+        self._parameters = charmm.CharmmParameterSet()
+
+    @property
+    def parameters(self: Self) -> charmm.CharmmParameterSet:
+        """Return the actual parameter object.
+
+        Returns
+        -------
+        charmm.CharmmParameterSet
+            Atom and bond types
+        """
+        return self._parameters
+
+    @property
+    def forces(self: Self) -> NDArray:
+        """Return the force constant data.
+
+        Returns
+        -------
+        numpy.ndarray
+            Force constant data
+        """
+        return np.asarray([bond_type.k for bond_type in self._parameters.bond_types.values()])
+
+    @forces.setter
+    def forces(self: Self, k: NDArray) -> None:
+        """Set the force constant data.
+
+        Parameters
+        ----------
+        k : numpy.ndarray
+            Force constant data
+        """
+        logger.debug("Updating the equilibrium force constants.")
+        try:
+            for bond_type, force in zip(self._parameters.bond_types, k, strict=True):
+                self._parameters.bond_types[bond_type].k = force
+        except ValueError as e:
+            logger.exception(e)
+            raise
+
+    @property
+    def distances(self: Self) -> NDArray:
+        """Return the equilibrium bond distance data.
+
+        Returns
+        -------
+        numpy.ndarray
+            Equilibrium bond distance data
+        """
+        return np.asarray([bond_type.req for bond_type in self._parameters.bond_types.values()])
+
+    @distances.setter
+    def distances(self: Self, req: NDArray) -> None:
+        """Set the equilibrium bond distance data.
+
+        Parameters
+        ----------
+        req : numpy.ndarray
+            Equilibrium bond distance data
+        """
+        logger.debug("Updating the equilibrium bond distances.")
+        try:
+            for bond_type, distance in zip(self._parameters.bond_types, req, strict=True):
+                self._parameters.bond_types[bond_type].req = distance
+        except ValueError as e:
+            logger.exception(e)
+            raise
+
+    def initialize(
+        self: Self, universe: mda.Universe, forces: NDArray | None = None, lengths: NDArray | None = None
+    ) -> None:
+        """Fill the parameters with atom types and bond information.
+
+        Parameters
+        ----------
+        universe : :class:`mda.Universe`
+            Universe with bond information
+        forces : :class:`numpy.ndarray`, optional
+            Force constants between bonded atoms
+        lengths : :class:`numpy.ndarray`, optional
+            Bond lengths between atoms
+
+        Raises
+        ------
+        MDAnalysis.NoDataError
+            if no bond data exists
+        ValueError
+            if number of bonds, force constants, or bond lengths do not match
+        """
+        # Prepare atom types
+        logger.debug("Adding atom types to the parameter list.")
+        atom_types: dict[str, pmd.AtomType] = {}
+        for atom in universe.atoms:
+            atom_type = pmd.AtomType(atom.type, -1, atom.mass, charge=atom.charge)
+            atom_type.epsilon = 0.0
+            atom_type.epsilon_14 = 0.0
+            atom_type.sigma = 0.0
+            atom_type.sigma_14 = 0.0
+            atom_type.number = -1
+            atom_types[atom.type] = atom_type
+        self._parameters.atom_types.update(atom_types)
+
+        # Setup atom bond types
+        if forces is not None and lengths is not None:
+            try:
+                logger.debug("Adding bond types to the parameter list.")
+                bond_keys = universe.bonds.topDict.keys()
+                bond_type = {
+                    key: pmd.BondType(k=force, req=length)
+                    for key, force, length in zip(bond_keys, forces, lengths, strict=True)
+                }
+                self._parameters.bond_types.update(bond_type)
+            except mda.NoDataError as e:
+                e.add_note("No bond data found in the universe.")
+                logger.exception(e)
+                raise
+            except ValueError as e:
+                e.add_note("Forces or lengths are not equal to the number of bonds.")
+                logger.exception(e)
+                raise
+
+    def write(
+        self: Self, par: str | Path | None = None, top: str | Path | None = None, stream: str | Path | None = None
+    ) -> None:
+        """Write the parameter data to a parameter, topology, or stream file.
+
+        Parameters
+        ----------
+        par : str or Path, optional
+            Parameter file
+        top : str or Path, optional
+            Topology file
+        stream : str or Path, optional
+            Stream file
+
+        Raises
+        ------
+        ValueError
+            if neither the parameter or stream filename is provided
+        """
+        if par is not None:
+            logger.info(f"Writing parameter file: {par}")
+        if top is not None:
+            logger.info(f"Writing topology file: {top}")
+        if stream is not None:
+            logger.info(f"Writing stream file: {stream}")
+
+        if not self._parameters.atom_types and not self._parameters.bond_types:
+            logger.warning("No atom types or bond types were provided. The parameter file will be empty.")
+
+        par = Path(par).as_posix() if par is not None else None
+        top = Path(top).as_posix() if top is not None else None
+        stream = Path(stream).as_posix() if stream is not None else None
+        self._parameters.write(par=par, top=top, stream=stream)
+
+    def read(self: Self, filename: str | Path) -> None:
+        """Read the parameters from a file.
+
+        Parameters
+        ----------
+        filename : str or Path
+            Parameter file
+        """
+        param_file = Path(filename)
+        try:
+            if param_file.suffix == ".par" or param_file.suffix == ".prm":
+                self._parameters.read_parameter_file(param_file.as_posix())
+            elif param_file.suffix == ".rtf":
+                self._parameters.read_topology_file(param_file.as_posix())
+            else:
+                self._parameters.read_stream_file(param_file.as_posix())
+        except FileNotFoundError as e:
+            logger.exception(e)
+            raise
+
+        if not self._parameters.atom_types or not self._parameters.bond_types:
+            message = "No atom types or bond types found in the parameter file"
+            logger.exception(message)
+            raise OSError(message)
---
+++ tests/io/test_charmm_parameter.py
@@ -0,0 +1,347 @@
+# ------------------------------------------------------------------------------
+#  fluctmatch
+#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+"""Test I/O for CHARMM parameter files."""
+
+from collections import OrderedDict
+from pathlib import Path
+from typing import Self
+
+import MDAnalysis as mda
+import numpy as np
+import pytest
+from fluctmatch.io.charmm_parameter import CharmmParameter
+from numpy import testing
+from pyfakefs import fake_file, fake_filesystem
+
+from tests.datafile import FLUCTDCD, FLUCTPSF, PRM, STR
+
+
+class TestCharmmParameter:
+    """Test CHARMM parameter file object."""
+
+    @pytest.fixture(scope="class")
+    def universe(self: Self) -> mda.Universe:
+        """Universe of an elastic network model.
+
+        Returns
+        -------
+        MDAnalysis.Universe
+            Elastic network model
+        """
+        return mda.Universe(FLUCTPSF, FLUCTDCD)
+
+    @pytest.fixture(scope="class")
+    def param_file(self: Self, fs_class: fake_filesystem.FakeFilesystem) -> fake_file.FakeFile:
+        """Return an empty file.
+
+        Parameters
+        ----------
+        fs_class : :class:`pyfakefs.fake_filesystem.FakeFileSystem`
+            Filesystem
+
+        Returns
+        -------
+        :class:`pyfakefs.fake_file.FakeFile
+            Empty file in memory
+        """
+        return fs_class.create_file("charmm.prm")
+
+    def test_initialize(self: Self, universe: mda.Universe) -> None:
+        """Test initialization of a parameter file.
+
+        GIVEN an elastic network model, bond force constants, and bond lengths
+        WHEN a parameter object is provided
+        THEN a parameter object is initialized.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            Elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        atom_types = OrderedDict({atom.type: "" for atom in universe.atoms})
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        testing.assert_equal(param._parameters.atom_types.keys(), atom_types.keys())
+        assert all(key in param._parameters.bond_types for key in universe.bonds.topDict)
+        testing.assert_allclose(
+            [at.mass for at in param._parameters.atom_types.values()], universe.atoms.masses, rtol=1e-4
+        )
+        testing.assert_allclose([bt.k for bt in param._parameters.bond_types.values()], forces, rtol=1e-4)
+        testing.assert_allclose([bt.req for bt in param._parameters.bond_types.values()], lengths, rtol=1e-4)
+
+    def test_initialize_unequal_size(self: Self, universe: mda.Universe) -> None:
+        """Test initialization of a parameter file with unequal sizes of forces and bond lengths.
+
+        GIVEN an elastic network model with bonds, bond force constants, and bond lengths
+        WHEN the three variables are of unequal size
+        THEN a ValueError is raised.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            Elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds) - 1, dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()[:-2]
+        param = CharmmParameter()
+
+        with pytest.raises(ValueError):
+            param.initialize(universe, forces=forces, lengths=lengths)
+
+    def test_initialize_no_bonds(self: Self) -> None:
+        """Test initialization of a parameter file with no bond data.
+
+        GIVEN an elastic network model with no bonds, bond force constants, and bond lengths
+        WHEN the three variables are of unequal size
+        THEN a parameter object is initialized.
+
+        """
+        universe = mda.Universe.empty(0)
+        forces = np.zeros(5, dtype=float)
+        param = CharmmParameter()
+
+        with pytest.raises(mda.NoDataError):
+            param.initialize(universe, forces=forces, lengths=forces)
+
+    def test_write(self: Self, universe: mda.Universe, param_file: fake_file.FakeFile) -> None:
+        """Test writing a parameter file.
+
+        GIVEN a universe and a parameter file
+        WHEN a parameter object is initialized
+        THEN a parameter object is written.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        tmp_path : Path
+            A parameter file
+        param_file : :class:`pyfakefs.fake_file.FakeFile
+            Empty file in memory
+        """
+        filename = Path(param_file.name)
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        param.write(par=param_file.name)
+        assert filename.exists()
+        assert filename.stat().st_size > 0
+
+    def test_write_fail(self: Self, universe: mda.Universe) -> None:
+        """Test writing a parameter file if no filenames are provided.
+
+        GIVEN a universe and no parameter file
+        WHEN a parameter object is initialized
+        THEN a ValueError is raised.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        tmp_path : Path
+            A parameter file
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        with pytest.raises(ValueError):
+            param.write()
+
+    def test_read(self: Self) -> None:
+        """Test reading a parameter file.
+
+        GIVEN a parameter file
+        WHEN a parameter object is initialized and read
+        THEN a parameter file is read and loaded into the parameter object.
+        """
+        param = CharmmParameter()
+        param.read(PRM)
+
+        assert len(param._parameters.atom_types) > 0, "Atom type definitions don't exist."
+        assert len(param._parameters.bond_types) > 0, "Bond definitions don't exist."
+
+    def test_read_no_file(self: Self) -> None:
+        """Test reading non-existent parameter file.
+
+        GIVEN a non-existent parameter filename
+        WHEN a parameter object is initialized and read
+        THEN a FileNotFoundError is raised.
+        """
+        param_file = "charmm.str"
+        param = CharmmParameter()
+        with pytest.raises(FileNotFoundError):
+            param.read(param_file)
+
+    def test_read_empty_file(self: Self) -> None:
+        """Test reading stream file without parameter information.
+
+        GIVEN a stream file without parameter information
+        WHEN a parameter object is initialized and read
+        THEN an OSError is raised.
+        """
+        param = CharmmParameter()
+        with pytest.raises(OSError):
+            param.read(STR)
+
+    def test_parameters_property(self: Self, universe: mda.Universe) -> None:
+        """Test parameters property.
+
+        GIVEN an elastic network model, equilibrium force constants, and bond lengths
+        WHEN a parameter object is initialized and the parameters property is called
+        THEN the underlying parameter object is returned.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        """
+        # Initialize parameters
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        # Test parameters getter
+        parameters = param.parameters
+        assert parameters.atom_types == param._parameters.atom_types
+        assert parameters.bond_types == param._parameters.bond_types
+
+    def test_forces_property(self: Self, universe: mda.Universe) -> None:
+        """Test getter/setter of forces property.
+
+        GIVEN an elastic network model, equilibrium force constants, and bond lengths
+        WHEN a parameter object is initialized and the forces property is called
+        THEN force constants are retrieved or set.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        # Test getter
+        data = param.forces
+        testing.assert_allclose(data, forces, rtol=1e-05, atol=1e-08, err_msg="Forces don't match.", verbose=True)
+
+        # Test setter
+        param.forces = np.zeros_like(forces)
+        testing.assert_allclose(param.forces, 0.0, rtol=1e-05, atol=1e-08, err_msg="Forces don't match.", verbose=True)
+
+    def test_forces_property_fail(self: Self, universe: mda.Universe) -> None:
+        """Test setter of forces property.
+
+        GIVEN an elastic network model, equilibrium force constants, and bond lengths
+        WHEN a parameter object is initialized and a shorter forces array is given
+        THEN a ValueError is raised.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        # Test setter
+        with pytest.raises(ValueError):
+            param.forces = np.zeros_like(forces)[:-1]
+
+    def test_distances_property(self: Self, universe: mda.Universe) -> None:
+        """Test getter/setter of distances property.
+
+        GIVEN an elastic network model, equilibrium force constants, and bond lengths
+        WHEN a parameter object is initialized and the distances property is called
+        THEN equilibrium bond distances are retrieved or set.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        # Test getter
+        data = param.distances
+        testing.assert_allclose(data, lengths, rtol=1e-05, atol=1e-08, err_msg="Distances don't match.", verbose=True)
+
+        # Test setter
+        param.distances = np.zeros_like(lengths)
+        testing.assert_allclose(
+            param.distances, 0.0, rtol=1e-05, atol=1e-08, err_msg="Distances don't match.", verbose=True
+        )
+
+    def test_distancess_property_fail(self: Self, universe: mda.Universe) -> None:
+        """Test setter of distances property.
+
+        GIVEN an elastic network model, equilibrium force constants, and bond lengths
+        WHEN a parameter object is initialized and a shorter distances array is given
+        THEN a ValueError is raised.
+
+        Parameters
+        ----------
+        universe : :class:`MDAnalysis.Universe`
+            An elastic network model
+        """
+        rng = np.random.default_rng()
+        forces = rng.random(len(universe.bonds), dtype=universe.bonds.values().dtype)
+        lengths = universe.bonds.values()
+        param = CharmmParameter()
+        param.initialize(universe, forces=forces, lengths=lengths)
+
+        # Test setter
+        with pytest.raises(ValueError):
+            param.forces = np.zeros_like(lengths)[:-1]
