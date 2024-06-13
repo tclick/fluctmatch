0c23f90cd9d78819b70a9676eacc002c229adbf2
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-06-13 14:07:46 -0500

    refactor: use first frame of trajectory if reference file not found
    Set reference to first frame of provided trajectory if no reference exists.
    Update documentation in both src and tests.
    Ensure that trajectory properly aligned.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- src/fluctmatch/commands/align.py
+++ src/fluctmatch/commands/align.py
@@ -1,35 +1,18 @@
-# ------------------------------------------------------------------------------
-#  fluctmatch
-#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+# ---------------------------------------------------------------------------------------------------------------------
+# fluctmatch
+# Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
 #
-#  All rights reserved.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met:
-#
-#  Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
-#
-#  Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#
-#  Neither the name of the author nor the names of its contributors may be used
-#  to endorse or promote products derived from this software without specific
-#  prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
-#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
-#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-#  DAMAGE.
-# ------------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# This program is distributed in the hope that it will be useful, # but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <[1](https://www.gnu.org/licenses/)>.
+# ---------------------------------------------------------------------------------------------------------------------
 # pyright: reportAttributeAccessIssue=false
 """Align a trajectory to its first frame or to a reference structure."""
 
@@ -44,6 +27,7 @@ from loguru import logger
 from MDAnalysis import transformations
 
 from fluctmatch import __copyright__
+from fluctmatch.commands import FILE_MODE
 from fluctmatch.libs.logging import config_logger
 
 
@@ -116,6 +100,7 @@ from fluctmatch.libs.logging import config_logger
     "--verbosity",
     default="INFO",
     show_default=True,
+    type=click.Choice("INFO DEBUG WARNING ERROR CRITICAL".split()),
     help="Minimum severity level for log messages",
 )
 @click.help_option("-h", "--help", help="Show this help message and exit")
@@ -129,26 +114,50 @@ def align(
     mass: bool,
     verbosity: str,
 ) -> None:
-    """Align the trajectory.
+    """Align a molecular dynamics trajectory to a reference structure.
+
+    This function performs an alignment of a trajectory against a reference structure based on the specified atom
+    selection. If a reference structure is not provided or cannot be found, alignment will occur using the first
+    frame of the provided trajectory. The alignment can be mass-weighted. The aligned trajectory is saved in the
+    specified output directory. Logging of the process is configurable through the verbosity level and the logfile path.
 
     Parameters
     ----------
-    topology : Path, default=$CWD/input.parm7
-        Topology file
-    trajectory : Path, default=$CWD/input.nc
-        Trajectory file
-    reference : Path, default=$CWD/ref.pdb
-        Reference structure
-    outdir : Path, default=$CWD
-        Output directory
-    logfile : Path, default=align.log
-        Location of log file
-    select : str, default=ca
-        Atom selection
-    mass : bool
-        Mass-weighted alignment
-    verbosity : str, default=INFO
-        Level of verbosity for logging output
+    topology : Path, default: $CWD/input.parm7
+        Path to the topology file.
+    trajectory : Path, default: $CWD/input.parm7
+        Path to the trajectory file.
+    reference : Path, default: $CWD/ref.pdb
+        Path to the reference structure file. If the reference does not exist, the first frame of the trajectory
+        will be used.
+    outdir : Path, default: $CWD/output
+        Path to the output directory where the aligned trajectory will be saved.
+    logfile : Path, default: $CWD/logging.log
+        Path to the log file for recording the process.
+    select : {'ca', 'all', 'cab', 'backbone'}
+        Atom selection type for alignment.
+    mass : bool, default: False
+        Perform a mass-weighted alignment if True.
+    verbosity : {'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'}
+        Logging verbosity level.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    To align 'simulation.nc' to 'reference.pdb' using C-alpha atoms for alignment and save the aligned trajectory in
+    the 'output' directory:
+
+    >>> align(topology=Path('topology.parm7'), trajectory=Path('simulation.nc'), reference=Path('reference.pdb'),
+    ...       outdir=Path('output'),  logfile=Path('alignment.log'), select='ca', mass=False, verbosity='INFO')
+
+    Notes
+    -----
+    The function initializes the logger to record messages to the specified log file and console. It selects atoms from
+    the trajectory and reference structure based on the provided selection criteria. A transformation is applied to
+    align the trajectory to the reference, and the aligned trajectory is written to the output directory.
     """
     config_logger(name=__name__, logfile=logfile, level=verbosity)
     click.echo(__copyright__)
@@ -160,12 +169,17 @@ def align(
         "backbone": "backbone or nucleicbackbone",
     }
     weight = "mass" if mass else None
+    outdir.mkdir(mode=FILE_MODE, parents=True, exist_ok=True)
     output = outdir.joinpath(f"aligned_{trajectory.name}")
     click.echo(output)
 
     universe = mda.Universe(topology, trajectory)
     mobile = universe.select_atoms(selection[select])
-    ref = mda.Universe(topology, reference).select_atoms(selection[select])
+    if reference.is_file():
+        ref = mda.Universe(topology, reference).select_atoms(selection[select])
+    else:
+        logger.warning("Reference file defaulting to the first frame of the trajectory.")
+        ref = mobile
 
     transform = transformations.fit_rot_trans(mobile, ref, weights=weight)
     universe.trajectory.add_transformations(transform)
@@ -173,5 +187,5 @@ def align(
     with mda.Writer(output.as_posix(), n_atoms=universe.atoms.n_atoms) as out:
         logger.info("Aligning trajectory to the reference structure.")
         for _ in universe.trajectory:
-            out.write(universe)
+            out.write(universe.atoms)
         logger.info(f"The structure has been aligned in {output}.")
--- tests/commands/test_align.py
+++ tests/commands/test_align.py
@@ -1,35 +1,18 @@
-# ------------------------------------------------------------------------------
-#  fluctmatch
-#  Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
+# ---------------------------------------------------------------------------------------------------------------------
+# fluctmatch
+# Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
 #
-#  All rights reserved.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met:
-#
-#  Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
-#
-#  Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#
-#  Neither the name of the author nor the names of its contributors may be used
-#  to endorse or promote products derived from this software without specific
-#  prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
-#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
-#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-#  DAMAGE.
-# ------------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# This program is distributed in the hope that it will be useful, # but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <[1](https://www.gnu.org/licenses/)>.
+# ---------------------------------------------------------------------------------------------------------------------
 # pyright: reportArgumentType = false
 """Tests for align of molecular dynamics trajectories."""
 
@@ -39,12 +22,26 @@ import os
 from pathlib import Path
 from typing import Self
 
+import MDAnalysis as mda
+import numpy as np
 import pytest
 from click.testing import CliRunner
 from fluctmatch.cli import main
-from MDAnalysisTests.datafiles import TPR, XTC
-from testfixtures.mock import Mock
-from testfixtures.replace import Replacer
+from MDAnalysis.analysis.rms import RMSD
+from MDAnalysisTests.datafiles import DCD2, PSF
+from testfixtures import TempDirectory
+
+
+@pytest.fixture()
+def reference() -> mda.Universe:
+    """Generate a reference structure.
+
+    Returns
+    -------
+    MDAnalysis.Universe
+        Reference structure
+    """
+    return mda.Universe(PSF, DCD2)
 
 
 class TestAlign:
@@ -78,26 +75,60 @@ class TestAlign:
         assert "Usage:" in result.output
         assert result.exit_code == os.EX_OK
 
-    def test_align(self, cli_runner: CliRunner) -> None:
-        """Test the alignment of a trajectory.
+    @pytest.mark.parametrize(
+        "selection", [("all", "all"), ("ca", "name CA"), ("cab", "name CA CB"), ("backbone", "backbone")]
+    )
+    def test_align(self, cli_runner: CliRunner, selection: tuple[str, str], reference: mda.Universe) -> None:
+        """Test the align function for proper trajectory alignment.
 
-        GIVEN the align subcommand
-        WHEN a topology and trajectory file are provided
-        THEN a new trajectory file should be written
+        GIVEN: Paths to the topology, trajectory, reference files, and output directory
+        WHEN: The align function is invoked with the specified atom selection and options
+        THEN: The output trajectory should be aligned according to the reference structure
+              and a log file should be created with the specified verbosity level.
+
+        This test will check if the align function correctly aligns the trajectory to the
+        reference structure and creates the expected output files with the correct content.
+        It will also verify that the log file is created and contains the expected messages.
+
+        The test will cover:
+        - Correct file creation for the aligned trajectory
+        - Proper logging in the specified log file
+        - Handling of the 'mass' option for mass-weighted alignment
+        - Function's response to various 'select' options for atom selection
 
         Parameters
         ----------
         cli_runner : CliRunner
             Command-line cli_runner
+        selection : str
+            Atom selection used for alignment between the trajectory and the reference structure.
+        reference : mda.Universe
+            Reference structure
         """
-        with cli_runner.isolated_filesystem() as ifs, Replacer() as replace:
-            tmp_path = Path(ifs)
-            log_file = tmp_path / "align.log"
+        with TempDirectory(create=True) as tempdir:
+            tmp_path = tempdir.as_path()
+            log_file = tmp_path.joinpath("align.log")
+            traj_file = tmp_path.joinpath(f"aligned_{Path(DCD2).name}")
 
-            mock_write = replace("MDAnalysis.coordinates.base.WriterBase.write", Mock())
             result = cli_runner.invoke(
-                main, f"align -s {TPR} -f {XTC} -r {XTC} -o {ifs} -l {log_file} -t backbone --mass"
+                main, f"align -s {PSF} -f {DCD2} -o {tmp_path} -l {log_file} -t {selection[0]} --mass"
             )
 
+            # Check outcome of CLI including file creation.
             assert result.exit_code == os.EX_OK
-            mock_write.assert_called()
+            assert log_file.is_file(), f"{log_file} does not exist"
+            assert traj_file.is_file(), f"{traj_file} does not exist"
+
+            # Compre the core of the protein as listed on the MDAnalysis example for r.m.s.d. calculations.
+            # https://docs.mdanalysis.org/stable/documentation_pages/analysis/rms.html
+            group_selection = [
+                f"{selection[1]} and (resid 1-29 or resid 60-121 or resid 160-214)",
+            ]
+            max_rmsd = 4.0
+            aligned = mda.Universe(PSF, traj_file.as_posix()).select_atoms(selection[1])
+            rmsd = (
+                RMSD(aligned, reference, select=selection[1], groupselections=group_selection, weights="mass")
+                .run()
+                .results.rmsd[:, 3]
+            )
+            assert np.all(rmsd < max_rmsd), f"r.m.s.d. should be less than {max_rmsd:.1f}."
