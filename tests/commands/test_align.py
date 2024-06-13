2904e18d7088e23974e9ffd5eaa695c8830b2ac2
Timothy H. Click, Ph.D. <thclick@umary.edu>
2024-06-13 15:35:07 -0500

    docs: update copyright to GPL v3+
    Change license to GNU GPL v3+ to match listed license.
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- tests/commands/test_align.py
+++ tests/commands/test_align.py
@@ -2,21 +2,24 @@
 # fluctmatch
 # Copyright (c) 2013-2024 Timothy H. Click, Ph.D.
 #
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# This file is part of fluctmatch.
+#
+# Fluctmatch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
-
-# This program is distributed in the hope that it will be useful, # but WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-# more details.
-
+#
+# Fluctmatch is distributed in the hope that it will be useful, # but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <[1](https://www.gnu.org/licenses/)>.
+#
+# Reference:
+# Timothy H. Click, Nixon Raj, and Jhih-Wei Chu. Simulation. Meth Enzymology. 578 (2016), 327-342,
+# Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics doi:10.1016/bs.mie.2016.05.024.
 # ---------------------------------------------------------------------------------------------------------------------
 # pyright: reportArgumentType = false
 """Tests for align of molecular dynamics trajectories."""
-
-from __future__ import annotations
 
 import os
 from pathlib import Path
