3da086a6ceb82260e7c6323eb545ff7752a9afb3
Timothy H. Click, Ph.D. <thclick@umary.edu>
2026-06-26 10:03:17 -0500

    chore(license): update copyright headers to reflect historical range
    Updates the copyright notice header blocks across multiple Python source
    files to explicitly span from the project's inception to the current year
    (2013-2026).
    
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) 2026 Timothy H. Click
+#  Copyright (c) 2013-2026 Timothy H. Click
 #  Project: fluctmatch
 #
 #  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
